a69dd430e0d5ffadf9bee874db157ebce2da9f4a
Natasha Alves <alvesna9999@gmail.com>
2018-07-27 20:15:21 -0300

    Add files via upload

--- AvaliadorAluno.java
+++ AvaliadorAluno.java
@@ -1,23 +1,39 @@
 package br.com.cariocadev.ProjetoMatrix;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 
 public class AvaliadorAluno {
+	
 
 	public BigDecimal getMaiorNota(BigDecimal[] notas) {
-		// Implementação o código aqui
-		
-		return null;
+		if(notas==null) {
+			throw new IllegalArgumentException("Entrada invalida");
+		}
+		else {
+			return System.out.println(new BigDecimal("2.00").max(new BigDecimal("1.5")));
 	}
+	   
 
 	public BigDecimal getMedia(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		// Implementação o código aqui
-		return null;
+		
+		if((nota1==null)|| (nota2==null)|| (nota3==null)|| (nota1<0)|| (nota2<0)|| (nota3<0)|| (nota1>10)|| (nota2>10)|| (nota3>10)) {
+			throw new IllegalArgumentException("Entrada invalida");
+		}
+		else {
+			System.out.println((new BigDecimal("4.00").add(new BigDecimal("5.00").add(new BigDecimal("7.00"))/3,2,RoundingMode.UP)));
+		}
+		return getMedia(nota1,nota2,nota3);
 	}
 
 	public String getStatus(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		// Implementação o código aqui
-		return null;
+	    else if(getMedia(nota1,nota2,nota3)>=6) {
+		   System.out.println("Aprovado");
+	    }
+	    else if(getMedia(nota1,nota2,nota3).compareTo(new )
+	}
+		return getStatus(nota1,nota2,nota3);
 	}
 	
 	
--- CalculadoraSalario.java
+++ CalculadoraSalario.java
@@ -1,22 +1,70 @@
 package br.com.cariocadev.ProjetoMatrix;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalculadoraSalario {
 
 	public BigDecimal getSalarioLiquido(BigDecimal salarioBruto, BigDecimal percentualImpostoINSS) {
-		// Implementar o código aqui
-		return null;
+		if((salarioBruto==null) || (percentualImpostoINSS==null)) {
+			throw new IllegalArgumentException("Entrada invalida");
+		}
+		else {
+			System.out.println(new salarioBruto.subtract(new percentualImpostoINSS));
+		}
+		
+		return getSalarioLiquido(salarioBruto,percentualImpostoINSS);
 	}
 
+	
+	
+	
 	public BigDecimal getValorINSS(BigDecimal salarioBruto) {
-		// Implementar o código aqui
-		return null;
+		String result = "invalido";
+		if(salarioBruto == null) {
+			throw new IllegalArgumentException();
+		
+		}
+		 else if(salarioBruto<=1693.72){
+			  
+			  return System.out.println(salarioBruto.multiply(new BigDecimal("0.08")),2,RoundingMode.UP);
+		 }
+		 else if((salarioBruto<=2822.90) && (salarioBruto>=1693.73)){
+			     
+			       return System.out.println(salarioBruto.multiply(new BigDecimal("0.09")),2,RoundingMode.UP);
+		    }
+		       else if(salarioBruto>=2822.91){
+				    
+				    return System.out.println(salarioBruto.multiply(new BigDecimal("0.11")),2,RoundingMode.UP);
+		       }
+		return getValorINSS(salarioBruto);
+		
 	}
 
+	
+	
+	
+	
 	public BigDecimal getValorPlanoDeSaude(Integer idade) {
-		// TODO Auto-generated method stub
-		return null;
+		if((idade==null) || (idade<0)) {
+			throw new IllegalArgumentException("Entrada invalida");
+		}
+		 else if((idade>=0) && (idade<=9))
+			System.out.println(new BigDecimal("75.00"));
+		 else if((idade>=10) && (idade<=19))
+		    System.out.println(new BigDecimal("112.50"));
+		 else if((idade>=20) && (idade<=29))
+		    System.out.println(new BigDecimal("168.75"));
+		 else if((idade>=30) && (idade<=39))
+		    System.out.println(new BigDecimal("253.13"));
+		 else if((idade>=40) && (idade<=49))
+	    	 System.out.println(new BigDecimal("379.69"));
+		 else if((idade>=50) && (idade<=59))
+	    	 System.out.println(new BigDecimal("569.54"));
+		 else if(idade>=60)
+	    	 System.out.println(new BigDecimal("854.30"));
+		          
+		return getValorPlanoDeSaude(idade);
 	}
 
 }
